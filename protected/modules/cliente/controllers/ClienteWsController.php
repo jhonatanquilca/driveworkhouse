66fbb07fd65a78abe7de3cdd25244586f9d3ade0
jhonatanquilca <jhonatand.quilca@gmail.com>
2015-09-16 23:24:53 -0500

    accion para el search de app android

--- protected/modules/cliente/controllers/ClienteWsController.php
+++ protected/modules/cliente/controllers/ClienteWsController.php
@@ -34,6 +34,35 @@ round((select if(ISNULL( sum(d.monto)),0, sum(d.monto)) from deuda d where d.cli
         Yii::app()->end();
     }
 
+    public function actionSearch($param) {
+//        select distinct(t.id), t.nombre, t.apellido, t.documento, t.telefono, t.celular, t.email_1, t.email_2,
+//        CONCAT(t.nombre, CONCAT(" ", t.apellido)) AS nombre_completo,
+//        round((select if(ISNULL( sum(d.monto)), 0, sum(d.monto)) from deuda d where d.cliente_id = t.id)-(select if(ISNULL( sum(p.monto)), 0, sum(p.monto)) from pago p where p.cliente_id = t.id), 2) as deuda
+//        from cliente t
+//        where CONCAT(t.nombre, CONCAT(" ", t.apellido)) LIKE "%j%" or t.documento LIKE "%j%"
+//        order by CONCAT(t.nombre, CONCAT(" ", t.apellido)) ASC
+        $model = new Cliente('search');
+        $array = array();
+
+        if (!empty($model->findAll())) {
+            $command = Yii::app()->db->createCommand()
+                    ->select('distinct(t.id), t.nombre, t.apellido, t.documento, t.telefono, t.celular, t.email_1, t.email_2,
+        CONCAT(t.nombre, CONCAT(" ", t.apellido)) AS nombre_completo,
+        round((select if(ISNULL( sum(d.monto)), 0, sum(d.monto)) from deuda d where d.cliente_id = t.id)-(select if(ISNULL( sum(p.monto)), 0, sum(p.monto)) from pago p where p.cliente_id = t.id), 2) as deuda')
+                    ->from('cliente t')
+                    ->where('CONCAT(t.nombre, CONCAT(" ", t.apellido)) LIKE :param or t.documento LIKE :param', array(':param' => '%' . $param . '%'))
+                    ->order('CONCAT(t.nombre, CONCAT(" ",t.apellido)) ASC');
+
+            $array['data'] = $command->queryAll();
+            $array['success'] = true;
+        } else {
+            $array['success'] = false;
+        }
+
+        print json_encode($array);
+        Yii::app()->end();
+    }
+
     public function actionView($id) {
         $model = Cliente::model()->findByPk($id);
         $array = array();
